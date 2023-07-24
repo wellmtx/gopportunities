7ede837d723b7c9bbc889c7a1db4450717ead803
wellmtx <wellingtonsaraivasousa@gmail.com>
2023-07-24 01:07:36 -0300

    feat: :sparkles: createOpening logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,13 +1,37 @@
 package handler
 
 import (
+	"gopportunities/schemas"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "POST Opening",
-	})
+	request := CreateOpeningRequest{}
+
+	c.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Validation error: %v", err.Error())
+		sendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("Error creating opening: %v", err.Error())
+		sendError(c, http.StatusInternalServerError, "Error creating opening on database")
+		return
+	}
+
+	sendSuccess(c, "create-opening", opening)
 }
