87c7480c99ffc68c8c732fb66979fd9813aa2070
wellmtx <wellingtonsaraivasousa@gmail.com>
2023-07-24 22:53:40 -0300

    feat: :sparkles: delete opening logic

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,34 @@
 package handler
 
 import (
+	"fmt"
+	"gopportunities/schemas"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "DELETE Opening",
-	})
+	id := c.Query("id")
+
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	err := db.First(&opening, id).Error
+
+	if err != nil {
+		sendError(c, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(c, http.StatusInternalServerError, fmt.Sprintf("error deleting opening with id: %s", id))
+		return
+	}
+
+	sendSuccess(c, "delete-opening", opening)
 }
