73187f98608b5faef069cba1ecabb0ad9939b98a
wellmtx <wellingtonsaraivasousa@gmail.com>
2023-07-26 01:37:46 -0300

    docs: :memo: add documentation for show opening

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -7,6 +7,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+
+// @Summary Show Opening
+// @Description Show a opening
+// @Tags Openings
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} ShowOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [get]
 func ShowOpeningHandler(c *gin.Context) {
 	id := c.Query("id")
 
